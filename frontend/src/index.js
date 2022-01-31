d9a6dcab632ed8b796018aa3f8161925492252fc
Michaela Chang <michaela.w.chang@gmail.com>
2022-01-30 23:46:49 -0800

    rough layout and components created

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './index.scss';
+import Navbar from './components/Navbar';
+import MealSection from './components/MealSection';
+import Header from './components/Header'
+import SearchBar from './components/SearchBar'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* TODO: Search Component */}
+    <SearchBar />
+
+    {/* TODO: Header Component */}
+    <Header locationName='USC Village Dining Hall' />
+    {/* TODO: MealSection Component */}
+        {/* TODO: MealItem Component */}
+    <MealSection stationName='Plant Based' />
+    <MealSection stationName='Flexiterian' />
+    {/* TODO: Navbar Component */}
+    <Navbar />
   </React.StrictMode>,
   document.getElementById('root')
 );
