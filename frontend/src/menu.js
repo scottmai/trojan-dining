03afaa1e7765a9db1651c63c472f7f678ef4c702
Beatrice Hahn <hahnb@usc.edu>
2022-02-11 18:14:43 -0800

    Update menu.js

--- frontend/src/menu.js
+++ frontend/src/menu.js
@@ -1,26 +1,17 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
 
-export default class menu extends Component {
-    render() {
-        return (
-            <div>
+export default function Menu() {
+    useEffect(() => {
 
-            </div>
-        )
-    }
+        async function fetchMenu(){
+            const menuRes = await axios.get('http://localhost:8000/menu/')
+            setMenu()(menuRes.data)
+        }
+        fetchMenu()
+    }, []);
+
 }
-
-useEffect(() => {
-    //REMOVE this later
-console.log('use effect ran');
-
-    async function fetchMenu(){
-        const menuResponse = await axios.get('http://localhost:8000/menu/')
-        FileSystemEntry(menuResponse.data)
-    }
-    fetchMenu()
-}, [])
 
 //npx json-server --watch db.json --port 8000
 //that creates endpoints:
@@ -28,15 +19,3 @@ console.log('use effect ran');
 //   /menu/{id} GET fetch a single menu
 //   /menu POST add a new menu
 //   /menu/{id} DELETE delete a blog
-
-useEffect(( => {
-    fetch('http://localhost:8000/menu')
-        //instead of async
-        .then(res => {
-            return res.json();
-        })
-        .then(data => {
-            console.log(data)
-            setMenu(data);
-        });
-}, []));
