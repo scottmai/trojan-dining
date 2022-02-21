b158fd4d5ae1c0f922e9c2b1b67734403bc4e30b
Brice Carter <bncarter@usc.edu>
2022-02-21 15:08:35 -0800

    Small changes to master branch that will allow it to deploy onto Heroku (#14)
    * added gunicorn to reqs in order to deploy to heroku also created a procfile
    
    * more deployment related changes
    
    * just added a comment to explain the inclusion of the settings at the end of settings
    
    * testing one more time before pull request

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -134,3 +134,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_ALLOW_ALL = True
 ALLOWED_HOSTS = ['*']
 
+# Static files (CSS, JavaScript, Images). This was required by Heroku
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra lookup directories for collectstatic to find static files
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+#  Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
