67f4b78a0184b3eadb4e2feec3a7aba4d08ecdd8
Brice Carter <bncarter@usc.edu>
2022-02-24 17:39:05 -0800

    mmm

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -136,9 +136,9 @@ CORS_ORIGIN_ALLOW_ALL = True
 ALLOWED_HOSTS = ['*']
 
 # Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, 'static'),
+# )
 
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
