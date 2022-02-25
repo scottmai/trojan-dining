75660f1f873ea246cba8d29bb818359725ad9cdc
Brice Carter <bncarter@usc.edu>
2022-02-24 17:42:24 -0800

    mmm

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -135,12 +135,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_ALLOW_ALL = True
 ALLOWED_HOSTS = ['*']
 
-# Extra lookup directories for collectstatic to find static files
-# STATICFILES_DIRS = (
-#     os.path.join(PROJECT_ROOT, 'static'),
-# )
-
-#  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
 django_heroku.settings(locals())
