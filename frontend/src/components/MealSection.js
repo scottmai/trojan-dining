e27815503c6a283e58e08c3eeadb64f918057b36
Michaela Chang <michaela.w.chang@gmail.com>
2022-03-14 19:51:24 -0700

    dropdown functionality added!

--- frontend/src/components/MealSection.js
+++ frontend/src/components/MealSection.js
@@ -1,23 +1,57 @@
 import MealItem from './MealItem'
 import dropdownIcon from '../assets/icons/dropdown.svg'
+import dropupIcon from '../assets/icons/dropup.svg'
+import React from 'react';
 
-const MealSection = (props) => {
+class MealSection extends React.Component {
 
-    return (
-        <div className="mealSection">
-            <button className="sectionTitle">
-                <img src={dropdownIcon} alt="dropdown" />
-                <span>{props.stations}</span>
-            </button>
-            <div className="dropdown-container">
-                {props.items.map(function(item) {
-                    return (
-                        <MealItem name={item.name} allergens={item.allergens} />
-                    );
-                })}
+    constructor(props) {
+        super(props);
+        this.state = {
+            toggled: false,
+        };
+    }
+
+    toggleDropdown = () => {
+        this.setState(prevState => ({
+            toggled: !prevState.toggled,
+        }));
+
+        if (this.state.toggled) {
+            
+        }
+    }
+
+    render() {
+        const toggled = this.state.toggled;
+        let dropdownContent;
+        let dropdownIconRender;
+        
+        if (toggled) {
+            dropdownIconRender =<img src={dropupIcon} alt="dropdown" />;
+            dropdownContent = (
+                <div className="dropdown-container">
+                    {this.props.items.map(function(item) {
+                        return (
+                            <MealItem name={item.name} allergens={item.allergens} />
+                        );
+                    })}
+                </div>
+            );
+        } else {
+            dropdownIconRender = <img src={dropdownIcon} alt="dropdown" />;
+        }
+
+        return (
+            <div className="mealSection">
+                <button className="sectionTitle" onClick={this.toggleDropdown}>
+                    {dropdownIconRender}
+                    <span>{this.props.stations}</span>
+                </button>
+                {dropdownContent}
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default MealSection;
