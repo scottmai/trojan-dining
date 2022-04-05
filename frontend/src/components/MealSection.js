8994463616e9e49443e21041985f1c573f40fec9
Michaela Chang <michaela.w.chang@gmail.com>
2022-04-04 18:06:47 -0700

    Changed MealSection to state hook

--- frontend/src/components/MealSection.js
+++ frontend/src/components/MealSection.js
@@ -1,53 +1,36 @@
 import MealItem from './MealItem'
 import dropdownIcon from '../assets/icons/dropdown.svg'
 import dropupIcon from '../assets/icons/dropup.svg'
-import React from 'react';
+import React, { useState } from 'react';
 
-class MealSection extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            toggled: false
-        };
-    }
-
-    toggleDropdown = () => {
-        this.setState(prevState => ({
-            toggled: !prevState.toggled
-        }));
-    }
-
-    render() {
-        const toggled = this.state.toggled;
-        let dropdownContent;
-        let dropdownIconRender;
-        
-        if (toggled) {
-            dropdownIconRender =<img src={dropupIcon} alt="dropdown" />;
-            dropdownContent = (
-                <div className="dropdown-container">
-                    {this.props.items.map(function(item) {
-                        return (
-                            <MealItem name={item.name} allergens={item.allergens} />
-                        );
-                    })}
-                </div>
-            );
-        } else {
-            dropdownIconRender = <img src={dropdownIcon} alt="dropdown" />;
-        }
-
-        return (
-            <div className="mealSection">
-                <button className="sectionTitle" onClick={this.toggleDropdown}>
-                    {dropdownIconRender}
-                    <span>{this.props.stations}</span>
-                </button>
-                {dropdownContent}
+const MealSection = ({props}) => {
+    const [toggled, toggle] = useState(true);
+    let dropdownContent;
+    let dropdownIconRender;
+    
+    if (toggled) {
+        dropdownIconRender =<img src={dropupIcon} alt="dropdown" />;
+        dropdownContent = (
+            <div className="dropdown-container">
+                {this.props.items.map(function(item) {
+                    return (
+                        <MealItem name={item.name} allergens={item.allergens} />
+                    );
+                })}
             </div>
         );
+    } else {
+        dropdownIconRender = <img src={dropdownIcon} alt="dropdown" />;
     }
-}
 
+    return (
+        <div className="mealSection">
+            <button className="sectionTitle" onClick={toggle(!toggled)}>
+                {dropdownIconRender}
+                <span>{this.props.stations}</span>
+            </button>
+            {dropdownContent}
+        </div>
+    );
+}
 export default MealSection;
