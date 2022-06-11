787630228d22b31a2bc9e4620d3bd3f9b4ba6212
Brice Carter <bncarter@usc.edu>
2022-06-10 17:51:31 -0700

    Menu items (#22)
    * weird assert bug
    
    * updated the save_menu function to now save menu items into a menu database that can be retrieved from the overall menu item by uuid
    
    * Just remembered to create the pull request. New save_menu function just dropped along with a associated function retrieve_menu that pulls a menu object from the database given a specific date. This function is useful since the menu objects stored in the database are not fully constructed and only have uuid references to the menuitems
    
    * removed test files
    
    * added command that saves todays menu to the database. This is necessary to run the scheduler on heroku
    
    * added text feedback for savetodaysmenu command
    
    * added connection string to settings.py
    
    * made changes that allow for environment variables
    
    * Update requirements.txt
    
    * Update requirements.txt
    
    * small changes
    
    * small changes
    
    * small changes
    
    * small changes
    
    * small changes
    
    * small changes
    
    * small changes
    
    * small changes
    
    * small changes
    
    * small changes
    
    * small changes
    
    * small changes
    
    * small changes
    
    * small changes
    
    * removed venv
    
    * recommitting
    
    * updated menu and retrieve_menu implementations. Also updated test
    
    Co-authored-by: Scott Mai <scottmai@usc.edu>

--- backend/trojan_dining/save_menu.py
+++ backend/trojan_dining/save_menu.py
@@ -1,26 +1,46 @@
+import uuid
 from trojan_dining.models import Menu, MenuItem
 import datetime
 
+# saves menu from database given a dictionary object
+# VERY IMPORTANT!: This changes the menu dict in place. So it may need to be necessary to create another menu dict depending on your 
+# uses
 
 def save_menu(dict_menu, menu_day=None):
     # function to persist a scrubbed menu to the database
     # create menu document
     menu_doc = Menu()
-    # populate it's meals attribute
+    # add uuids to items in  and persist menuitems to menuitem collection with uuids
+    for meal in dict_menu:
+        for hall in meal['dining_halls']:
+            for station in hall['stations']:
+                station['item_uuids'] = []
+
+                # populating the item_uuids field
+                for item in station['items']:
+                    station['item_uuids'].append({'uuid': str(uuid.uuid4())})
+
+                for index, item in enumerate(station['items']):
+                    # retrieve item uuid from the item_uuids array in station
+                    item['item_uuid'] = station['item_uuids'][index]['uuid']
+
+                    # persist the menu item to the database
+                    menu_item = MenuItem()
+                    menu_item.name = item['name']
+                    menu_item.item_uuid = item['item_uuid']
+                    menu_item.allergens = item['allergens']
+                    menu_item.save()
+
+                # get rid of array of menu item objects on the station object
+                station.pop('items')
+    # populate menu doc's meals attribute
     menu_doc.meals = dict_menu
+
     # add which day the menu reffers to
     if(menu_day is None):
         menu_doc.date = datetime.datetime.now()
     else:
         menu_doc.date = menu_day
+
     # Just add water (persist the menu to the database)
     menu_doc.save()
-
-    for meal_time in dict_menu:
-        for hall in meal_time['dining_halls']:
-            for station in hall['stations']:
-                for item in station['items']:
-                    menu_item_doc = MenuItem()
-                    menu_item_doc.name = item['name']
-                    menu_item_doc.allergens = item['allergens']
-                    menu_item_doc.save()
