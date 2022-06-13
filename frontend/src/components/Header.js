bf1fa84a1c7690b2ccc5e66efbc3867ed0eddbc3
Michaela Chang <32281640+milkcee12@users.noreply.github.com>
2022-06-12 21:47:04 -0700

    Style menu (#21)
    * dropdown functionality added!
    
    * Completed allergen color coding
    
    * Changed MealSection to state hook
    
    * Moved from react inheritance to using state hooks
    
    * Basic datepicker added
    
    * Small syntax updates basted on PR review
    
    * Fixed merge branch errors
    
    * Fixed MealSection titles not appearing

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,23 @@
+import React, {useState} from "react";
+import Form from "react-bootstrap/Form";
+
 const Header = ({locationName}) => {
+    const today = new Date();
+    const [displayDate, setDisplayDate] = useState(today);
     return (
         <div className="header row">
             <h2 className="col-7">{locationName}</h2>
-            
-            {/* TODO: Calendar link */}
-            <button className="col">January 20, 2022</button>
+            <div className="col-5 date-picker">
+                <Form.Group controlId="menu_date" size="sm">
+                <Form.Control
+                    type="date"
+                    name="menu_date"
+                    value={displayDate.getDate}
+                    defaultValue={today.getDate}
+                    onChange={(e) => setDisplayDate(e)}
+                />
+                </Form.Group>
+            </div>
         </div>
     );
 }
--- frontend/src/components/MealSection.js
+++ frontend/src/components/MealSection.js
@@ -1,21 +1,29 @@
 import MealItem from './MealItem'
 import dropdownIcon from '../assets/icons/dropdown.svg'
+import dropupIcon from '../assets/icons/dropup.svg'
+import React, { useState } from 'react';
 
 const MealSection = (props) => {
+    const [isOpen, setIsOpen] = useState(true);
+
+    const toggleDropdown = () => setIsOpen(!isOpen);
 
     return (
         <div className="mealSection">
-            <button className="sectionTitle">
-                <img src={dropdownIcon} alt="dropdown" />
+
+            <button className="sectionTitle" onClick={toggleDropdown}>
+                <img src={isOpen ? dropdownIcon : dropupIcon} alt="dropdown" />
                 <span>{props.stations}</span>
             </button>
-            <div className="dropdown-container">
-                {props.items.map(function(item) {
+
+            <div className={`dropdown-container ${isOpen ? 'open' : ''}`}>
+                {props.items.map((item) => {
                     return (
                         <MealItem name={item.name} allergens={item.allergens} />
                     );
                 })}
             </div>
+            
         </div>
     );
 }
