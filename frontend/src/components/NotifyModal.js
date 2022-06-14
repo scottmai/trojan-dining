7ea7b3236f6a207e2dba3bee475c4a959b2b9c72
Beatrice (Con) Hahn <hahnb@usc.edu>
2022-06-13 23:53:54 -0700

    inline style

--- frontend/src/components/NotifyModal.js
+++ frontend/src/components/NotifyModal.js
@@ -1,37 +1,37 @@
 import React, {useState} from 'react';
+import ReactModal from 'react-modal';
 import Modal from 'react-modal';
-import img from "./meal.jpg";
 
-const modalStyles = {
+
+const customStyles = {
     overlay: {
         zIndex: 8,
         background: 'rgba(0, 0, 0, 0.80)',
     },
     content: {
-        top: '150px',
-        left: '120px',
-        right: '120px',
-        bottom: '150px',
+        top: '20%',
+        left: '20%',
+        right: '20%',
+        bottom: '20%',
     }
 };
 
- function NotifyModal() {
+ReactModal.setAppElement('#root');
+
+function NotifyModal() {
       const [modalIsOpen, setModalIsOpen] = useState(false)
       return (
-       <div class="notify-modal">
-            <button class="notify-btn" onClick={() => setModalIsOpen(true)}>Notify Me!</button>
+       <div className="notify-modal">
+            <button className="notify-btn" onClick={() => setModalIsOpen(true)}>Notify Me!</button>
             <Modal 
                 isOpen={modalIsOpen} 
                 onRequestClose={() => setModalIsOpen(false)} 
-                style={modalStyles}
+                style={customStyles}
             >
-                <img class="meal-img" src={img} alt="platter of food"/>
+                <button className="close-btn" onClick={() => setModalIsOpen(false)}>X</button>
                 <div>
                     <h3>Want to get updated?</h3>
                     <p>Sign up to get alerts every time your favorite meal is featured at the dining hall!</p>
-                </div>
-                <div>
-                    <button onClick={() => setModalIsOpen(false)}>X</button>
                 </div>
             </Modal>
         </div>
