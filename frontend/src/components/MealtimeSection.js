8d5f28b4c1379b3a96d95a026e48dcbd03ea39da
Michaela Chang <michaela.w.chang@gmail.com>
2022-06-26 21:37:29 -0700

    Mealtime dropdowns and styling complete

--- frontend/src/components/MealtimeSection.js
+++ frontend/src/components/MealtimeSection.js
@@ -1,18 +1,33 @@
 import DiningLocation from './DiningLocation'
 import React, { useState } from 'react';
+import dropdownIcon from '../assets/icons/chevron-meals.svg'
 import breakfastIcon from "../assets/icons/sunrise-dark.svg";
+import lunchIcon from "../assets/icons/sun.svg";
+import dinnerIcon from "../assets/icons/moon.svg";
 
 const MealtimeSection = (props) => {
     const [isOpen, setIsOpen] = useState(true);
 
     const toggleDropdown = () => setIsOpen(!isOpen);
 
+    const mealtimeIcon = () => {
+        switch(props.mealtime.name) {
+            case 'Breakfast':
+                return breakfastIcon;
+            case 'Lunch':
+                return lunchIcon;
+            default:
+                return dinnerIcon;
+        }
+    }
+
     return (
         <div className="mealtimeSection">
             <div className="mealtime-banner row" onClick={toggleDropdown}>
                 <h2 className="mealtimeTitle">
+                <img className={`dropdown-icon ${isOpen ? '' : 'closed'}`} src={dropdownIcon} alt="dropdown" />
                     {props.mealtime.name}
-                    <img className="mealtime-icon" alt="Breakfast" src={breakfastIcon} />
+                    <img className={`mealtime-icon ${props.mealtime.name.toLowerCase()}`} alt={props.mealtime.name} src={mealtimeIcon()} />
                 </h2>
             </div>
             
