d7b01807bb2610b5ae7a7640993f294acf816789
Beatrice Hahn <hahnb@usc.edu>
2022-06-30 12:39:25 -0700

    Notification form (added style ideas, not perfect yet) (#23)
    * Set up basic modal component
    
    * Add react-icons, spring, and styled-components
    
    * Simplified modal name
    
    * Added Notification Component and styles to pop up
    
    * Delete files that were in wrong directory
    
    * Reinstall react-modal to frontend directory
    
    * Align notification button to right side
    
    * Replace previous  modal using react-modal, styling
    
    * Lower modal component placement
    
    * Delete comment because it's already in readme
    
    * Fix overlay issue
    
    * Delete meal.jpg
    
    * inline style
    
    * testing
    
    * Add extra line at the end, delete unused button
    
    * Change to auto
    
    * Update yarn.lock

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
-
 import Header from './Header';
 import MealSection from './MealSection';
 import Navbar from './Navbar';
 import SearchBar from './SearchBar'
 import axios from 'axios';
+import NotifyModal from './NotifyModal'
 
 export default function Menu() {
     const [menu, setMenu] = useState(null);
@@ -36,6 +36,7 @@ export default function Menu() {
             </div>
             {menu != null && menu.length > 0
                 ? <div className="container-fluid menuItems">
+                    <NotifyModal/>
                     <h1 className="mealtimeTitle">Breakfast</h1>
                     {menu[0].dining_halls[0].stations.map(function (stations) {
                         return (
@@ -48,6 +49,4 @@ export default function Menu() {
         </div>
     )
 }
-
-//npx json-server --watch db.json --port 8000
 
---
+++ frontend/src/components/NotifyModal.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+import ReactModal from 'react-modal';
+import Modal from 'react-modal';
+
+
+const customStyles = {
+    overlay: {
+        zIndex: 8,
+        background: 'rgba(0, 0, 0, 0.80)',
+    },
+    content: {
+        margin: 'auto'
+    }
+};
+
+ReactModal.setAppElement('#root');
+
+function NotifyModal() {
+      const [modalIsOpen, setModalIsOpen] = useState(false)
+      return (
+       <div className="notify-modal">
+            <button className="notify-btn" onClick={() => setModalIsOpen(true)}>Notify Me!</button>
+            <Modal 
+                isOpen={modalIsOpen} 
+                onRequestClose={() => setModalIsOpen(false)} 
+                style={customStyles}
+            >
+                <button className="close-btn" onClick={() => setModalIsOpen(false)}>X</button>
+                <div>
+                    <h3>Want to get updated?</h3>
+                    <p>Sign up to get alerts every time your favorite meal is featured at the dining hall!</p>
+                </div>
+            </Modal>
+        </div>
+    )
+}
+
+export default NotifyModal;
