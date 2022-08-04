a7fe1b553c93cbd1a42c62eb7bfce81c9bb45c5c
Brice Carter <bncarter@usc.edu>
2022-08-03 20:02:23 -0500

    notifications endpoints added (#37)
    * notifications endpoints added
    
    * small fix
    
    * newlines added
    
    * removed unecessary conditional

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = str(os.getenv('SECRET_KEY'))
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'True' == str(os.getenv('DEBUG'))
+DEBUG = str(os.getenv('DEBUG')) == 'True'
 
 # Application definition
 
