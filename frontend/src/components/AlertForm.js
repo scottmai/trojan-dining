aef7b7321ec7fc54e930ab0190c6349858c72816
Beatrice (Con) Hahn <hahnb@usc.edu>
2022-08-24 22:14:48 -0700

    subscribe form UI (#38)
    * Use bootstrap to update email and phone UI
    
    * Change Header content to match design
    
    * Update button color and form field color to match design
    
    * Placeholder until subscribed food is selected
    
    * Add margins, remove required from phone field
    
    * Replace previous close button 'X' with svg X icon
    
    * add end line
    
    * Update close button styling
    
    * Make name of subscribed food item be in red
    
    * Update header font weight and mute sub text
    
    * Match border radius of form control with subscribe btn
    
    * Checkbox set to defaultChecked, simplify description
    
    * Update subscribe btn hex
    
    * Get rid of btn-primary, reposition subscribe btn
    
    * Update form header text content to match design
    
    * Renamed button class, added right margin and slim down padding
    
    * Remove inline Modal styling, replace with ReactModal class styling, add transition
    
    * Bold modal content title with sans-serif due to Roboto font issue
    
    * Adjust email field margin to match phone field
    
    * Reduce modal title bottom margin and modal height
    
    * Lighten overlay opacity
    
    * Use bootstrap to update email and phone UI
    
    * Change Header content to match design
    
    * Update button color and form field color to match design
    
    * Placeholder until subscribed food is selected
    
    * Add margins, remove required from phone field
    
    * Replace previous close button 'X' with svg X icon
    
    * add end line
    
    * Update close button styling
    
    * Make name of subscribed food item be in red
    
    * Update header font weight and mute sub text
    
    * Match border radius of form control with subscribe btn
    
    * Checkbox set to defaultChecked, simplify description
    
    * Update subscribe btn hex
    
    * Get rid of btn-primary, reposition subscribe btn
    
    * Update form header text content to match design
    
    * Renamed button class, added right margin and slim down padding
    
    * Remove inline Modal styling, replace with ReactModal class styling, add transition
    
    * Bold modal content title with sans-serif due to Roboto font issue
    
    * Adjust email field margin to match phone field
    
    * Reduce modal title bottom margin and modal height
    
    * Lighten overlay opacity
    
    * Replaced x icon
    
    * Remove unused import
    
    * Change placeholder text

--- frontend/src/components/AlertForm.js
+++ frontend/src/components/AlertForm.js
@@ -1,36 +1,21 @@
-//import React, { useEffect, useState } from 'react';
-import React, { useState } from 'react';
+import React from 'react';
 
 export default function AlertForm() {
-    const [inputValue, setInputValue] = useState('');
-    const handleInput = (e) => {
-        const formattedPhoneNumber = formatPhoneNumber(e.target.value);
-        setInputValue(formattedPhoneNumber);
-    };
-    
     return (
         <form>
-            <label>
-                Email:
-                <input type="email" name="email" />
-            </label>
-            <label>
-                Phone Number:
-                <input onChange={e => handleInput(e)} value={inputValue} />
-            </label>
-            <input type="submit" value="Sign up" />
+            <div className="form-group mt-3">
+                <label for="userEmail" className="text-muted">Email</label>
+                <input type="email" className="form-control" id="email" aria-describedby="emailForm" placeholder="tommytrojan@usc.edu" required/>
+            </div>
+            <div className="form-check">
+                <input type="checkbox" className="form-check-input" id="emailCheckbox" defaultChecked/>
+                <small className="form-text text-muted">Receive email notifications too</small>
+            </div>
+            <div className="form-group mt-3">
+                <label for="userPhone" className="text-muted">Phone</label>
+                <input type="tel" className="form-control" id="tel" aria-describedby="phoneForm" placeholder="123-456-7890" pattern="^\d{4}-\d{3}-\d{4}$" />
+            </div>
+            <button type="submit" className="btn subscribeBtn mt-4 position-absolute end-0">Subscribe</button>
         </form>
     )
-}
-
-function formatPhoneNumber(value) {
-    if (!value) return value;
-    const phoneNumber = value.replace(/[^\d]/g, '');
-    const phoneNumberLength = phoneNumber.length;
-    if (phoneNumberLength < 4) return phoneNumber;
-    if (phoneNumberLength < 7) {
-        return `(${phoneNumber.slice(0,3)}) ${phoneNumber.slice(3)}`;
-    }
-    return `(${phoneNumber.slice(0,3)}) ${phoneNumber.slice(3,6,
-        )}-${phoneNumber.slice(6,10)}`;
 }
