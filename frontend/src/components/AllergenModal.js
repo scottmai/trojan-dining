22ddae4ada343a3146f2132d5dde669ba6f6a64f
Michaela Chang <michaela.w.chang@gmail.com>
2022-08-29 10:16:50 -0700

    Allergen key modal created and styled

---
+++ frontend/src/components/AllergenModal.js
@@ -0,0 +1,81 @@
+import React from "react";
+
+const AllergenModal = (props) => {
+
+    const onClose = (e) => {
+        props.onClose && props.onClose(e);
+    }
+
+    if (!props.show) {
+        return null;
+    }
+
+    return (
+        <div className="allergenModal">
+            <div className="modal-bg" onClick={onClose}> </div>
+            <div className={`container ${props.show ? "modal-open" : ""}`}>
+                <button class="close-button" onClick={onClose}>X</button>
+                <div className="row allergen-list justify-content-center">
+                    <div className="col-10">
+                        <h2>Allergen Key</h2>
+                        <ul>
+                            <li>
+                                <span className="allergen-dairy">&#11044;</span>
+                                Dairy
+                            </li>
+                            <li>
+                                <span className="allergen-eggs">&#11044;</span>
+                                Eggs
+                            </li>
+                            <li>
+                                <span className="allergen-fish">&#11044;</span>
+                                Fish
+                            </li>
+                            <li>
+                                <span className="allergen-food-not-analyzed-for-allergens">&#11044;</span>
+                                Food Not Analyzed for Allergens
+                            </li>
+                            <li>
+                                <span className="allergen-peanuts">&#11044;</span>
+                                Peanuts
+                            </li>
+                            <li>
+                                <span className="allergen-pork">&#11044;</span>
+                                Pork
+                            </li>
+                            <li>
+                                <span className="allergen-sesame">&#11044;</span>
+                                Sesame
+                            </li>
+                            <li>
+                                <span className="allergen-shellfish">&#11044;</span>
+                                Shellfish
+                            </li>
+                            <li>
+                                <span className="allergen-soy">&#11044;</span>
+                                Soy
+                            </li>
+                            <li>
+                                <span className="allergen-tree-nuts">&#11044;</span>
+                                Tree Nuts
+                            </li>
+                            <li>
+                                <span className="allergen-vegan">&#11044;</span>
+                                Vegan
+                            </li>
+                            <li>
+                                <span className="allergen-vegeterian">&#11044;</span>
+                                Vegetarian
+                            </li>
+                            <li>
+                                <span className="allergen-gluten">&#11044;</span>
+                                Wheat/Gluten
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default AllergenModal;
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,24 +1,31 @@
 import React, {useState} from "react";
 import Moment from 'moment';
+import AllergenModal from './AllergenModal';
 
 const Header = () => {
     const [startDate, setStartDate] = useState(new Date());
+    const [showAllergenModal, setAllergenModal] = useState(false);
     Moment.locale('en');
 
+    const toggleAllergenModal = () => setAllergenModal(!showAllergenModal);
+
     return (
-        <div className="container-fluid header">
-            <div className="row justify-content-center align-items-center">
-                <h5 className="col text-center header-title">Trojan Dining</h5>
-            </div>
-            <div className="row justify-content-center">
-                <div className="datepicker col-4">
-                    <input type="date" id="datepicker" name="meal-date"
-                        value={Moment(startDate).format('YYYY-MM-DD')} min={Moment(startDate).format('YYYY-MM-DD')} />
+        <div className="header">
+            <div className="container-fluid">
+                <div className="row justify-content-center align-items-center">
+                    <h5 className="col text-center header-title">Trojan Dining</h5>
                 </div>
-                <div className="legend col-4">
-                    <button onClick={void(0)}>Allergen Key</button>
+                <div className="row justify-content-center">
+                    <div className="datepicker col-4">
+                        <input type="date" id="datepicker" name="meal-date"
+                            value={Moment(startDate).format('YYYY-MM-DD')} min={Moment(startDate).format('YYYY-MM-DD')} />
+                    </div>
+                    <div className="legend col-4">
+                        <button onClick={toggleAllergenModal}>Allergen Key</button>
+                    </div>
                 </div>
             </div>
+            <AllergenModal onClose={toggleAllergenModal} show={showAllergenModal} />
         </div>
     );
 }
