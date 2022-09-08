94265c04be5fa8c201ccbbaef8cd92110d80d31d
Scott Mai <scottmai702@gmail.com>
2022-09-07 17:12:15 -0700

    rm unused import

--- frontend/src/components/NotifyModal.js
+++ frontend/src/components/NotifyModal.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react'
+import React, { useContext } from 'react'
 import ReactModal from 'react-modal'
 import AlertForm from './AlertForm'
 import xIcon from "../assets/icons/altx.png"
