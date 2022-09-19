71c4ea1966c7ba09fef5ea3c2ca70a7fab5f601d
Scott Mai <scottmai702@gmail.com>
2022-09-19 02:10:52 -0700

    subscriptions page UI

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import bellIcon from "../assets/icons/bell-white.png";
 
 const Navbar = () => {
@@ -7,9 +8,11 @@ const Navbar = () => {
             <div className="row justify-content-evenly text-align-center">
                 <button className="col col-md-12 location">Village</button>
                 <button className="col col-md-12 mealtime">Lunch</button>
-                <button className="col-2 col-md-12 notifications">
-                    <img src={bellIcon} alt="Notification settings" />
-                </button>
+                <Link to="/subscriptions">
+                    <button className="col-2 col-md-12 notifications">
+                        <img src={bellIcon} alt="Notification settings" />
+                    </button>
+                </Link>
             </div>
         </div>
     );
--- frontend/src/components/subscriptions/SubscriptionsPage.jsx
+++ frontend/src/components/subscriptions/SubscriptionsPage.jsx
@@ -1,12 +1,45 @@
-import { useSearchParams } from "react-router-dom";
+import './subscriptions.scss';
+import { Link, useNavigate, useSearchParams } from "react-router-dom";
+import chevronLeft from '../../assets/icons/chevron-left.svg';
+import pencilSquare from '../../assets/icons/pencil-square.svg';
+import { useState } from 'react';
+import axios from 'axios';
+import { useMemo } from 'react';
+import SubscriptionsList from './SubscriptionsList';
+import EmailForm from './EmailForm';
 
 const SubscriptionsPage = () => {
     const [searchParams] = useSearchParams();
     const email = searchParams.get('email');
+    const [userEmail, setUserEmail] = useState(email ?? '');
+    const [showEmailForm, setShowEmailForm] = useState(!userEmail);
+    const handleUpdateEmail = email => {
+        setUserEmail(email);
+        setShowEmailForm(false);
+    }
+    const handleUserEmailClick = () => {
+        if (!showEmailForm) setShowEmailForm(true);
+    }
+
     return (
-        <div>
-            <h1>Subscriptions</h1>
-            <div>for {email}</div>
+        <div className="subscriptions">
+            <div className="subscriptions-header">
+                <Link to="/" className="back-btn">
+                    <img src={chevronLeft} alt="icon" width="28" />
+                </Link>
+                <div>
+                    <div className="title">Notifications settings</div>
+                    <div className="user-email" onClick={handleUserEmailClick}>
+                        {userEmail}
+                    </div>
+                </div>
+            </div>
+            <div className="subscriptions-content">
+                {showEmailForm
+                    ? <EmailForm initialEmail={userEmail} onUpdateEmail={handleUpdateEmail} />
+                    : <SubscriptionsList />
+                }
+            </div>
         </div>
     );
 }
