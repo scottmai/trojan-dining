f926dee00bce40f56bd44fed8325e0bec20eb05d
Michaela Chang <michaela.w.chang@gmail.com>
2022-10-03 11:03:42 -0700

    Basic preloader added

---
+++ frontend/src/components/Preloader.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Preloader() {
+    return (
+        <div className="preloader">
+            {/* Yoinked from Codepen: https://codepen.io/Treno/pen/vYJwQXw */}
+            <div className='wrapper'>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+    )
+}
