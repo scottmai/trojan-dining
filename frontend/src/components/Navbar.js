99c1a12beb5cb58fccee9c84c5d2144142bca732
Michaela Chang <michaela.w.chang@gmail.com>
2022-10-09 18:56:22 -0700

    Fixed red navbar bootstrap issues

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -6,10 +6,10 @@ const Navbar = () => {
     return (
         <div className="container-fluid nav-red d-md-flex align-items-center">
             <div className="row justify-content-evenly text-align-center">
-                <button className="col col-md-12 location">Village</button>
-                <button className="col col-md-12 mealtime">Lunch</button>
-                <Link to="/subscriptions">
-                    <button className="col-2 col-md-12 notifications">
+                <button className="col-5 col-md-12 location">Village</button>
+                <button className="col-5 col-md-12 mealtime">Lunch</button>
+                <Link className="col-2 col-md-12 notifications text-center" to="/subscriptions">
+                    <button>
                         <img src={bellIcon} alt="Notification settings" />
                     </button>
                 </Link>
